1a17fb7ebcab31e3b0ec1174a4d36ec3c8091599
Vasileps <sVasileps@gmail.com>
2024-01-26 19:03:08 +0300

    outputDir changed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir: 'docs',
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/ScheduleVue/" : "/",
 })
